a7d975e8d2584e0078d8890895190398622571c5
Viktorius Ando Saputra, S.Kom. <viktoriusando@gmail.com>
2026-01-10 01:21:33 +0700

    fiks ighg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.png'], // Pastikan nama file ini sesuai dengan di folder public
+      manifest: {
+        name: 'SKU Master Inventory',
+        short_name: 'SKU Master',
+        description: 'Aplikasi Stok Toko Acan',
+        theme_color: '#2563eb',
+        background_color: '#ffffff',
+        display: 'standalone',      // Wajib agar jadi App
+        orientation: 'portrait',    // Wajib kunci potret
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo.png', // Vite akan otomatis mencari ini di folder public
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512', // Pastikan file asli minimal segini
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    })
+  ],
 })
